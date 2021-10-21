8e341b99349f56e7c3d23180580883912b5437ca
Maiky-ZnfC <look2012a@gmail.com>
2021-10-21 18:33:33 -0500

    pa v.0.1.1.1
    Separación de código sql: 1) Definición de tablas 2) Inicialización de datos de las tablas

---
+++ mikuyta_init.sql
@@ -0,0 +1,9 @@
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`ProductoCod`, `Descripcion`, `Precio`, `Stock`) VALUES
+(1, 'Zapatos', '20.00', 4),
+(2, 'Polos', '10.00', 20),
+(3, 'Poleras', '15.00', 18),
+(4, 'Zapatillas', '20.00', 5);
--- mikuyta_tablas.sql
+++ mikuyta_tablas.sql
@@ -35,20 +35,6 @@ CREATE TABLE `producto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `producto`
---
-
-INSERT INTO `producto` (`ProductoCod`, `Descripcion`, `Precio`, `Stock`) VALUES
-(1, 'Zapatos', '20.00', 4),
-(2, 'Polos', '10.00', 20),
-(3, 'Poleras', '15.00', 18),
-(4, 'Zapatillas', '20.00', 5);
-
---
--- Índices para tablas volcadas
---
-
---
 -- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
